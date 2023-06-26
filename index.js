5b26d96b5b570c14279ae95485326fe8365ed6df
Marsh1100 <margie.b.agu17@gmail.com>
2023-06-26 17:34:02 -0500

    feat: :construction:

--- index.js
+++ index.js
@@ -33,6 +33,16 @@ class QuestionManager {
 // Crear instancia de Preguntas
 const questionManager =  new QuestionManager();
 
+//AddEventListener
+/*document.addEventListener('DOMContentLoaded',function(){
+  questionManager = JSON.parse(localStorage.getItem('questionManager')) || questionManager;
+  renderQuestions();
+});
+
+function addlocalStorage(){
+  localStorage.setItem('questionManager', JSON.stringify(questionManager));
+};*/
+
 //Interacción con el DOM
 const $questionForm = document.getElementById("question-form");
 
@@ -45,8 +55,11 @@ const $answerD = document.getElementById("answer_d");
 const $answerCorrect = document.getElementById("answer_correct");
 
 const $questionsList = document.getElementById("quiz");
+const $questionTest = document.getElementById("test");
 
 const $btnQuestion = document.getElementById("btn-question");
+const $btnView = document.getElementById("btn-view");
+
 //Formulario
 $btnQuestion.addEventListener("click", (e)=>{
     e.preventDefault();
@@ -61,7 +74,6 @@ $btnQuestion.addEventListener("click", (e)=>{
       const answerD = $answerD.value;
 
       const answerCorrect = $answerCorrect.value;
-      console.log(answerCorrect);
       const newQuestion = new Question(question,answerA,answerB,answerC,answerD,answerCorrect);
       
       if(e.target.textContent == 'Agregar pregunta'){
@@ -81,14 +93,22 @@ $btnQuestion.addEventListener("click", (e)=>{
       $answerD.value = " ";
       $answerCorrect.value = "Seleccionar respuesta";
 
-      console.log(questionManager)
-      console.log(newQuestion)
     }else{
       alert("No ha seleccionado ninguna opción de respuesta.")
     };
     
 
+});
+//AddEventListener
+
+$btnView.addEventListener('click',(e)=>{
+  e.preventDefault();
+
+  //Ocultar cuestionario y preguntas
+
+  renderQuiz();
 })
+
 
 function shuffle(unshuffled){
 
@@ -100,45 +120,86 @@ function shuffle(unshuffled){
   return shuffled;
 }
 
+
+
 function renderQuestions(){
   $questionsList.innerHTML=" ";
   const questions = questionManager.getQuestions();
+  console.log(questions)
+  for(let i = 0; i < questions.length; i++){
+    const item = questions[i];
 
+    let html = `<div class="answersQuiz">
+                <br><span>${(i+1)+"."+item.question}</span>
+                <div class="form-check">
+                <input value="A" class="form-check-input" type="radio" name="select"  disabled>
+                <label class="form-check-label">${item.answers[0]}</label>
+                </div>
+
+                <div class="form-check">
+                <input value="B"  class="form-check-input" type="radio" name="select" disabled  >
+                <label class="form-check-label">${item.answers[1]}</label>
+                </div>
+
+                <div class="form-check">
+                <input value="C" class="form-check-input" type="radio" name="select" disabled >
+                <label class="form-check-label">${item.answers[2]}</label>
+                </div>
+
+                <div class="form-check">
+                <input value="D" class="form-check-input" type="radio" name="select" disabled >
+                <label class="form-check-label">${item.answers[3]}</label>
+                </div>
+                <div class="botones">
+                <button  class="btn btn-warning" onclick="editQuestion(${i})">Editar</button>
+                <button  class="btn btn-danger" onclick="removeQuestion(${i})">Eliminar</button>
+                </div>
+                </div>
+                `;
+    $questionsList.insertAdjacentHTML('beforeend',html)
+  }
+};
+
+function renderQuiz(){
+  $questionTest.innerHTML=" ";
+  let questions = questionManager.getQuestions();
   for(let i = 0; i < questions.length; i++){
     const item = questions[i];
 
     //Array desordenado
     let answers2 = shuffle(item.answers);
     console.log(answers2);
-    let html = `<br><span>${(i+1)+"."+item.question}</span>
+    let html = `<div class="answersQuiz">
+                <br><span>${(i+1)+". "+item.question}</span>
                 <div class="form-check">
-                <input value="A" class="form-check-input" type="radio" name="select"  >
+                <input value="${answers2[0]}" class="form-check-input" type="radio" name="select"  >
                 <label class="form-check-label">${answers2[0]}</label>
                 </div>
 
                 <div class="form-check">
-                <input value="B"  class="form-check-input" type="radio" name="select"  >
+                <input value="${answers2[1]}"  class="form-check-input" type="radio" name="select"  >
                 <label class="form-check-label">${answers2[1]}</label>
                 </div>
 
                 <div class="form-check">
-                <input value="C" class="form-check-input" type="radio" name="select"  >
+                <input value="${answers2[2]}" class="form-check-input" type="radio" name="select"  >
                 <label class="form-check-label">${answers2[2]}</label>
                 </div>
 
                 <div class="form-check">
-                <input value="D" class="form-check-input" type="radio" name="select"  >
+                <input value="${answers2[3]}" class="form-check-input" type="radio" name="select"  >
                 <label class="form-check-label">${answers2[3]}</label>
                 </div>
-                <div class="botones">
-                <button  class="btn btn-warning" onclick="editQuestion(${i})">Editar</button>
-                <button  class="btn btn-danger" onclick="removeQuestion(${i})">Eliminar</button>
+                
                 </div>
                 `;
 
-    $questionsList.insertAdjacentHTML('beforeend',html)
+    $questionTest.insertAdjacentHTML('beforeend',html)
   }
-};
+  //addlocalStorage();
+
+  };
+
 
 function removeQuestion(index) {
   questionManager.removeQuestion(index);
